a8935a3ba0c4300b2060bad1e13b114f94e89460
lucas <lucasrsillva2000@gmail.com>
2023-08-02 08:44:37 -0300

    second commit

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+let c = 1
+do {
+  // faça o escreva na tela 'tudo bem!' e acrescente mais 1 no final
+  console.log('Tudo bem!')
+  c++ // c = c + 1
+} while (c <= 6) //enquanto o contador for menor ou igual a 6
+
+/*
+let c = 1
+while (c <= 3) {
+  //enquanto o 1 for menor que 6, escreva na tela 'tudo bem!'
+  console.log('Tudo bem!')
+  c++ // c = c + 1
+}
+ */
---
+++ aula14/ambiente.js
@@ -0,0 +1,12 @@
+/*
+let c = 1
+while(c<=10) {
+console.log('Olá')       
+c++
+}
+Código while
+*/
+
+for (let c = 1; c <= 10; c++) {
+  console.log('Olá')
+} //Código for
