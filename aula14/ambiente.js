06ea3abede0b19286c1b872fa976cea822483505
lucas <lucasrsillva2000@gmail.com>
2023-08-02 08:57:13 -0300

    adding lesson14

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -7,6 +7,14 @@ c++
 Código while
 */
 
+/*
 for (let c = 1; c <= 10; c++) {
   console.log('Olá')
 } //Código for
+*/
+
+console.log('Vai começar...')
+for (var c = 1; c <= 4; c++) {
+  console.log(c)
+}
+console.log('FIM!')
